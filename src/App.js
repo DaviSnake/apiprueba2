9bb2d01b39b08079bb3cfb8e0fc39a3a4c2dbcf9
DaviSnake <dmedinac@gmail.com>
2024-06-14 07:03:09 -0400

    codigo inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { useFetch } from "./useFetch";
+import { fetchData } from "./fetchData";
+import "./App.css";
+
+const apiData = fetchData("https://jsonplaceholder.typicode.com/todos");
 
 function App() {
+  const data = apiData.read();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch like a Pro</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul className="card">
+          {data?.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      </Suspense>
     </div>
   );
 }
-
-export default App;
+export default App; 
